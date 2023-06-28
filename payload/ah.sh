4cfbc6497fb91b5aeb5a58ec4e438358a659ca06
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-06-28 03:47:56 +0100

    Update ah.sh

--- payload/ah.sh
+++ payload/ah.sh
@@ -18,4 +18,19 @@
 #chown root:root /etc/resolv.conf
 #echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" >>  /etc/resolv.conf
 ###########################################
-/usr/bin/supervisord -n -c  /etc/supervisor/supervisord.conf
+#/usr/bin/supervisord -n -c  /etc/supervisor/supervisord.conf
+ffmpeg -re\
+      -i  https://pull-f5-gcp01.tiktokcdn.com/stage/stream-3283807712798310572_or4.flv \
+      -pix_fmt yuvj420p\
+      -x264-params keyint=48:min-keyint=48:scenecut=-1\
+      -b:v 4500k\
+      -b:a 128k\
+      -ar 44100\
+      -acodec aac\
+      -vcodec libx264\
+      -preset medium\
+      -crf 28\
+      -threads 4\
+      -f flv\
+      rtmp://a.rtmp.youtube.com/live2/ms9z-mx5s-fvjg-9tmf-93ap
+
